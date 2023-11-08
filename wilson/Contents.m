72a06558dad763dab2ae7063543c3e80cfad9f06
allthatsounds <clara.hollomey@allthatsounds.com>
2023-11-08 18:33:46 +0100

    wilson adjustments

--- wilson/Contents.m
+++ wilson/Contents.m
@@ -2,21 +2,22 @@
 %
 %  Peter L. Søndergaard, 2007 - 2023.
 %
-%  Wilson bases and WMDCT
+%  Basic transform routines
 %    DWILT          -  Discrete Wilson transform
 %    IDWILT         -  Inverse discrete Wilson transform
 %    DWILT2         -  2-D Discrete Wilson transform
 %    IDWILT2        -  2-D inverse discrete Wilson transform
-%    WMDCT          -  Modified Discrete Cosine transform
-%    IWMDCT         -  Inverse |wmdct|
-%    WMDCT2         -  2-D |wmdct|
-%    IWMDCT2        -  2-D inverse |wmdct|
 %    WIL2RECT       -  Rectangular layout of Wilson coefficients
 %    RECT2WIL       -  Inverse of |wil2rect|
 %    WILWIN         -  Evaluate Wilson window
 %    DWILTLENGTH    -  Length of Wilson/WMDCT system to expand a signal
 %
-%  Reconstructing windows and condition numbers
+%    WMDCT          -  Modified Discrete Cosine transform
+%    IWMDCT         -  Inverse |wmdct|
+%    WMDCT2         -  2-D |wmdct|
+%    IWMDCT2        -  2-D inverse |wmdct|
+%
+%  Reconstruction windows and bounds
 %    WILORTH        -  Window of Wilson/WMDCT orthonormal basis
 %    WILDUAL        -  Riesz dual window of Wilson/WMDCT basis 
 %    WILBOUNDS      -  Frame bounds of Wilson basis
